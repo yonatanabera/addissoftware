4e7dee41eec076c638e1f37b90fe2e75d2c81fc5
Yonatan Abera <yonatanabera2007@gmail.com>
2021-10-21 11:54:47 +0300

    Front end, redux, redux-saga, typescript, styled components is integrated

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const url = "mongodb://localhost/addissoftware";
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+app.use(cors({
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+  origin: '*'
+}));
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+//      next();
+// });
 
 mongoose.connect(url, { useNewUrlParser: true });
 
@@ -15,12 +35,16 @@ app.use(express.json());
 
 const employeeRouter = require("./routes/Employee");
 app.use("/employee", employeeRouter);
+
+app.use(bodyParser.json());
+
+
 app.use("/", (req, res) => {
   res.json({
-    "goto": 'http://localhost:3000/employee'
+    goto: `http://localhost:3001/employee`,
   });
 });
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("server started");
 });
--- backend/routes/Employee.js
+++ backend/routes/Employee.js
@@ -1,66 +1,66 @@
 const express = require("express");
 const router = express.Router();
-const Employee= require("../models/Employees");
+const Employee = require("../models/Employees");
 
 
-router.get("/", async(req, res) => {
-  try{
-    const employees =  await Employee.find();
+router.get("/", async (req, res) => {
+  try {
+    const employees = await Employee.find();
     res.json(employees);
-  }catch(err){
-      res.send('Error' + err);
+  } catch (err) {
+    res.status(400).send("Error" + err);
   }
 });
 
-router.post("/", async (req, res)=>{
-    const employee= new Employee({
-        name: req.body.name, 
-        birthday: req.body.birthday,
-        gender: req.body.gender,
-        salary: req.body.salary
-    })
+router.post("/", async (req, res) => {
+  const employee = new Employee({
+    name: req.body.name,
+    birthday: req.body.birthday,
+    gender: req.body.gender,
+    salary: req.body.salary,
+  });
 
-    try{
-        const a1=await employee.save(); 
-        res.json(a1);
-    }catch(err){
-        res.send("Error " + err)
-    }
+  try {
+    const a1 = await employee.save();
+    res.json(a1);
+  } catch (err) {
+    res.status(400).send("Error " + err);
+  }
 });
 
-router.get("/:id", async (req, res)=>{
-    try{
-        const employee= await Employee.findById(req.params.id);
-        res.json(employee);
-    }catch(err){
-        res.send("Error " + err)
-    }
-})
+router.get("/:id", async (req, res) => {
+  try {
+    const employee = await Employee.findById(req.params.id);
+    res.json(employee);
+  } catch (err) {
+    res.status(400).send("Error " + err);
+  }
+});
 
-router.patch("/:id", async (req, res)=>{
-    try{
-        const employee= await Employee.findById(req.params.id)
-        // Change all elemenets
-        employee.name= req.body.name;
-        employee.birthday= req.body.birthday;
-        employee.gender= req.body.gender;
-        employee.salary= req.body.salary;
-        const a1= await employee.save()
-        res.json(a1)
-    }catch(err){
-        res.send("Error " + err)
-    }
-})
+router.put("/:id", async (req, res) => {
+  try {
+    const employee = await Employee.findById(req.params.id);
+    employee.name = req.body.name;
+    employee.birthday = req.body.birthday;
+    employee.gender = req.body.gender;
+    employee.salary = req.body.salary;
+    const a1 = await employee.save();
+    res.json(a1);
+  } catch (err) {
+    res.status(400).send("Error " + err);
+  }
+});
 
 router.delete("/:id", async (req, res) => {
-    try{
-        const employee= await Employee.findById(req.params.id);
-        const name=employee.name;
-        employee.delete();
-        res.send("Deleted " + name);
-    }catch(err){
-        res.send("Error " + err)
-    }
-})
+  try {
+    console.log("ok");
+    const employee = await Employee.findById(req.params.id);
+    const name = employee.name;
+    employee.delete();
+    res.json({ Deleted: name });
+  } catch (err) {
+    res.status(400).send("Error " + err);
+  }
+});
 
 module.exports = router;
